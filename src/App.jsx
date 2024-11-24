baa2a143645b0e626e132d8f9293ac76e45edc30
katariyamansi25 <katariyamansi25@gmail.com>
2024-11-24 21:33:45 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-      <p className="read-the-docs">
-        Hello World
-      </p>
-    </>
+    <div className="min-h-screen flex items-center justify-center bg-pink-200">
+      <div className="flex flex-col gap-4">
+        <button className="px-6 py-3 text-white bg-gradient-to-r from-blue-500 to-blue-700 rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-110 hover:from-purple-600 hover:to-purple-600">
+          Button 1
+        </button>
+        <button className="px-6 py-3 text-white bg-gradient-to-r from-blue-500 to-blue-700 rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-110 hover:from-purple-600 hover:to-purple-600">
+          Button 2
+        </button>
+        <button className="px-6 py-3 text-white bg-gradient-to-r from-blue-500 to-blue-700 rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-110 hover:from-purple-600 hover:to-purple-600">
+          Button 3
+        </button>
+      </div>
+    </div>
   )
 }
 
